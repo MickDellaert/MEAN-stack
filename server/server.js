9ffb79402fc5d42dff53a83ee96de80643197f56
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-08-06 10:51:35 +0200

    setting up mongoose connection

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
-const port = 3000;
+const dbConnection = 'mongodb+srv://mick:meandbmick@cluster0.xt8j9.mongodb.net/friendbook?retryWrites=true&w=majority'
+
+const port = 9055;
+
+mongoose.connect(dbConnection, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err))
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}) );
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World')
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
