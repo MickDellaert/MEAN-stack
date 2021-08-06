5bb299bb5bd79dd8887b4b875acca5d7d1c5c378
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-08-06 11:47:58 +0200

    tried another way of setting up connection

--- server/server.js
+++ server/server.js
@@ -3,13 +3,24 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
-const dbConnection = 'mongodb+srv://mick:meandbmick@cluster0.xt8j9.mongodb.net/friendbook?retryWrites=true&w=majority'
-
 const port = 9055;
 
+const dbConnection = 'mongodb+srv://mick:meandbmick@cluster0.xt8j9.mongodb.net/friendbook?retryWrites=true&w=majority'
+
 mongoose.connect(dbConnection, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => console.log('connected to db'))
+    .then((result) => console.log('connected to database'))
     .catch((err) => console.log(err))
+
+// -- Another way of making an connection using db.on and db.once --
+// -- With this method promise is ignored todo --
+// mongoose.connect('mongodb+srv://mick:meandbmick@cluster0.xt8j9.mongodb.net/friendbook?retryWrites=true&w=majority\'', {useNewUrlParser: true, useUnifiedTopology: true});
+//
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//     console.log('connected!');
+// });
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
