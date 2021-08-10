a6319004cc747ae573f9fe091a37360370ec2761
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-08-10 10:41:43 +0200

    adding updated client files

---
+++ client/friendbook/src/app/components/form/form.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import {Friend} from "../../friend";
+import {AddFriendService} from "../../services/add-friend.service";
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+
+export class FormComponent implements OnInit {
+
+  allFriends: any;
+
+  // this array gets used in the select options dropdown in app.component.html
+  languages = [{name: "HTML"}, {name: "CSS"}, {name: "JavaScript"}, {name: "PHP"}]
+
+  // instantiate a new friendModel object using the Friend class defined in friend.ts
+  friendModel = new Friend("", "", "", "", "");
+
+
+  constructor(private addFriendService: AddFriendService) {
+  }
+
+  ngOnInit(): any {
+    this.getFriends('http://localhost:9055/allFriends').then();
+  }
+
+  // need to look into subscribe TODO
+  submitDetails() {
+    console.log(this.friendModel);
+    let observable = this.addFriendService.addFriend(this.friendModel);
+    observable.subscribe(
+      data => console.log("it worked"),
+      error => console.log("it didn't work"));
+
+    // I added an empty then at the end to make the error 'Promise returned from getFriends is ignored' go away
+    this.getFriends('http://localhost:9055/allFriends').then();
+  }
+
+
+  async getFriends(url: string): Promise<any> {
+    let response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    let data = await response.json();
+    this.allFriends = data;
+
+    return this.allFriends;
+  }
+}
---
+++ client/friendbook/src/app/friend.ts
@@ -0,0 +1,21 @@
+
+export class Friend {
+  firstname: string;
+  lastname: string;
+  email: string;
+  phonenumber: string;
+  language: string;
+
+  constructor(firstname: string,
+              lastname: string,
+              email: string,
+              phonenumber: string,
+              language: string)
+  {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email = email;
+    this.phonenumber = phonenumber;
+    this.language = language;
+  }
+}
